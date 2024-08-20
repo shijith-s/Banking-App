3414112aada90ea31fd20ee6f2fe75af05259111
Shijith <shijith.s@optym.com>
2024-08-20 18:28:10 +0530

    Implement state management. Store state in localstorage for persisting

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const STORAGE_KEY = "STORAGE_KEY";
+
 const updateElement = (id, text) => {
   let element = document.getElementById(id);
   element.textContent = text;
@@ -15,7 +17,24 @@ const getTransactionRow = (transaction) => {
   return row;
 };
 
+// =============== State Management ==================
+
+let state = Object.freeze({
+  ACCOUNT: null,
+});
+
+const updateState = (property, newData) => {
+  state = Object.freeze({
+    ...state,
+    [property]: newData,
+  });
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state.ACCOUNT));
+};
+
+// ============================================================
+
 const updateDashboard = () => {
+  let ACCOUNT = state.ACCOUNT;
   if (!ACCOUNT) return navigate("/login");
   updateElement("balance", ACCOUNT.balance.toFixed(2));
   updateElement("description", ACCOUNT.description);
@@ -32,12 +51,8 @@ const updateDashboard = () => {
 
 const routes = {
   "/login": { templateId: "login" },
-  "/dashboard": { templateId: "dashboard", init: updateDashboard },
+  "/": { templateId: "dashboard", init: updateDashboard },
 };
-
-// =============== State Management ==================
-
-let ACCOUNT = null;
 
 const updateView = () => {
   const path = window.location.pathname;
@@ -68,6 +83,21 @@ const navigate = (path) => {
   updateView();
 };
 
+const init = () => {
+  const savedAccount = localStorage.getItem(STORAGE_KEY);
+  if (savedAccount) {
+    updateState("ACCOUNT", JSON.parse(savedAccount));
+  }
+
+  // Whenever the url changes, update the view
+  window.onpopstate = () => updateView();
+
+  // Update view automatically whenever loaded initially
+  updateView();
+};
+
+init();
+
 const onLinkClick = (e) => {
   // Prevents the default behaviour of anchor tag
   e.preventDefault();
@@ -77,18 +107,17 @@ const onLinkClick = (e) => {
   navigate(url);
 };
 
-// Whenever the url changes, update the view
-window.onpopstate = () => updateView();
-
-// // Update view automatically whenever loaded initially
-updateView();
-
 // ================= Functions for registering and login ==================
 
 const login = async () => {
   let loginForm = document.getElementById("loginForm");
   let user = loginForm.user.value;
   getAccountDetails(user);
+};
+
+const logout = () => {
+  updateState("ACCOUNT", null);
+  navigate("/login");
 };
 
 const getAccountDetails = async (user) => {
@@ -104,8 +133,8 @@ const getAccountDetails = async (user) => {
   result.transactions = result.transactions.sort((a, b) =>
     a.date < b.date ? 1 : -1
   );
-  ACCOUNT = result;
-  navigate("/dashboard");
+  updateState("ACCOUNT", result);
+  navigate("/");
 };
 
 const register = async () => {
@@ -137,6 +166,7 @@ const register = async () => {
 };
 
 const addTransaction = async () => {
+  let ACCOUNT = state.ACCOUNT;
   if (!ACCOUNT.user) {
     return console.log("No user logged in!");
   }
@@ -168,6 +198,7 @@ const addTransaction = async () => {
   transactionForm.reset();
 };
 const deleteTransaction = async (id) => {
+  let ACCOUNT = state.ACCOUNT;
   if (!ACCOUNT.user) {
     return console.log("No user logged in!");
   }
